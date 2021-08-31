f440baeaafdd7569e256a47b16f201c427461e90
hitu69 <hiteshmarwaha24@gmail.com>
2021-08-31 19:51:05 +0530

    oka

--- frontend/src/components/layout/Landing.js
+++ frontend/src/components/layout/Landing.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import svgart from "../../img/undraw.svg";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="landingSection">
+      <div className="container">
+        <div className="landing-main">
+          <div className="left">
+            <h1>Nameless</h1>
+            <h6>Anonymous.Post.Interact</h6>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste
+              consequuntur sapiente quod similique? Mollitia cum consectetur
+              repellendus minus quasi nemo?
+            </p>
+
+            <button className="btn btn-primary">Register</button>
+          </div>
+          <div className="right">
+            <img src={svgart} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Logo from '../../img/logo.PNG';
+import React from "react";
+import Logo from "../../img/Nameless-Title-Only-Black.png";
 
 const Navbar = () => {
   return (
