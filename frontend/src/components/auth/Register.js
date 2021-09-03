9dcbe3421df0fb64442dfe9e004adc92f023b5b2
AYUSHBANSAL-hub <forstudyonly101@gmail.com>
2021-09-03 23:40:08 +0530

    register ka bg lagaya part 1

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,32 +1,34 @@
-import React from "react";
-
+import React, { Fragment } from 'react';
 const Register = () => {
   return (
-    <div className="center">
-      <div className="register-card">
-        <h1>Sign Up</h1>
-        <form>
-          <div className="form-item">
-            <input type="text" placeholder="Username" />
-          </div>
-          <div className="form-item">
-            <input type="text" placeholder="Email" />
-          </div>
-          <div className="form-item">
-            <input type="text" placeholder="College" />
-          </div>
-          <div className="form-item">
-            <input type="text" placeholder="Password" />
-          </div>
-          <div className="form-item">
-            <input type="text" placeholder="Confirm Password" />
-          </div>
-          <button className="btn btn-primary" type="submit" onClick="">
-            Register
-          </button>
-        </form>
+    <Fragment>
+      <div className='sign-up-bg'></div>
+      <div className='center'>
+        <div className='register-card'>
+          <h1>Sign Up</h1>
+          <form>
+            <div className='form-item'>
+              <input type='text' placeholder='Username' />
+            </div>
+            <div className='form-item'>
+              <input type='text' placeholder='Email' />
+            </div>
+            <div className='form-item'>
+              <input type='text' placeholder='College' />
+            </div>
+            <div className='form-item'>
+              <input type='text' placeholder='Password' />
+            </div>
+            <div className='form-item'>
+              <input type='text' placeholder='Confirm Password' />
+            </div>
+            <button className='btn btn-primary' type='submit' onClick=''>
+              Register
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
