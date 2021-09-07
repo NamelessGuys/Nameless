95903cdbde0eee733268b2662b607b99ed322c9f
AYUSHBANSAL-hub <forstudyonly101@gmail.com>
2021-09-07 17:08:03 +0530

    dropdown is final and links are added

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
 import Logo from '../../img/Nameless-Title-Only-Black.png';
+import { FaUser, FaCog } from 'react-icons/fa';
+import { FiLogOut } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 const guestLinks = (
   <ul>
     <li>
-      <Link to="/register">Register</Link>
+      <Link to='/register'>Register</Link>
     </li>
     <li>
-      <Link to="/login" className="btn btn-primary">
+      <Link to='/login' className='btn btn-primary'>
         Login
       </Link>
     </li>
@@ -19,19 +21,44 @@ const authLinks = (
   <Fragment>
     <ul>
       <li>
-        <Link to="/feed">Feed</Link>
+        <Link to='/feed'>Feed</Link>
       </li>
       <li>
-        <Link to="/chatrooms">Chatrooms</Link>
+        <Link to='/chatrooms'>Chatrooms</Link>
       </li>
+
       <li>
-        <div className="nav-profile">
-          <img
-            class="nav-profile-img"
-            src="https://randomuser.me/api/portraits/men/1.jpg"
-            alt="Profile Pic"
-          />
-          <h3 className="nav-username">Lorem Ipsum</h3>
+        <div class='dropdown'>
+          <button class='dropbtn'>
+            <div className='nav-profile'>
+              <img
+                class='nav-profile-img'
+                src='https://randomuser.me/api/portraits/men/1.jpg'
+                alt='Profile Pic'
+              />
+              <h3 className='nav-username'>Lorem Ipsum</h3>
+            </div>
+          </button>
+          <div className='dropdown-content'>
+            <a href='#'>
+              <i>
+                <FaUser />
+              </i>
+              Profile
+            </a>
+            <a href='#'>
+              <i>
+                <FaCog />
+              </i>
+              Settings
+            </a>
+            <a href='#'>
+              <i>
+                <FiLogOut />
+              </i>
+              Sign Out
+            </a>
+          </div>
         </div>
       </li>
     </ul>
@@ -40,10 +67,10 @@ const authLinks = (
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-graydark">
-      <div className="navbar-brand">
-        <Link to="/">
-          <img src={Logo} alt="Nameless" />
+    <nav className='navbar bg-graydark'>
+      <div className='navbar-brand'>
+        <Link to='/'>
+          <img src={Logo} alt='Nameless' />
         </Link>
       </div>
       {/* {guestLinks} */}
