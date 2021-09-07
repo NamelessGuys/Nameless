3d0a1b30cdd32bcf1783de3866a231a32a74d651
Gaurav Bansal <bansalgaurav726@gmail.com>
2021-09-07 16:44:00 +0530

    Added Nav Links and set up mongo server

--- config/db.js
+++ config/db.js
@@ -7,8 +7,6 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
     });
     console.log('MongoDB Connected...');
   } catch (err) {
--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../img/Nameless-Title-Only-Black.png';
 
 const guestLinks = (
   <ul>
     <li>
-      <a href="#">Register</a>
+      <Link to="/register">Register</Link>
     </li>
     <li>
-      <button className="btn btn-primary">Login</button>
+      <Link to="/login" className="btn btn-primary">
+        Login
+      </Link>
     </li>
   </ul>
 );
@@ -16,10 +19,10 @@ const authLinks = (
   <Fragment>
     <ul>
       <li>
-        <a href="#">Feed</a>
+        <Link to="/feed">Feed</Link>
       </li>
       <li>
-        <a href="#">Chatrooms</a>
+        <Link to="/chatrooms">Chatrooms</Link>
       </li>
       <li>
         <div className="nav-profile">
@@ -39,7 +42,9 @@ const Navbar = () => {
   return (
     <nav className="navbar bg-graydark">
       <div className="navbar-brand">
-        <img src={Logo} alt="Nameless" />
+        <Link to="/">
+          <img src={Logo} alt="Nameless" />
+        </Link>
       </div>
       {/* {guestLinks} */}
       {authLinks}
