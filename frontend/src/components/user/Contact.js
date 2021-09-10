450d7db7aeff1b127fcf7e653cc8289b0fe6e8c6
hitu69 <hiteshmarwaha24@gmail.com>
2021-09-10 22:35:53 +0530

    contact page

--- frontend/src/components/user/Contact.js
+++ frontend/src/components/user/Contact.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import '../../css/user.css';
+import React from "react";
+import "../../css/user.css";
 
 const Contact = () => {
   return (
@@ -7,10 +7,28 @@ const Contact = () => {
       <h2 className="large">Contact Us</h2>
       <div className="contact-content">
         <div className="contact-header">
-          <h2>Organisation:</h2>
-          <a href="mailto:nameless.care@gmail.com">Nameless</a>
+          <h4>Organisation</h4>
+          <a href="mailto:nameless.care@gmail.com">nameless.care@gmail.com</a>
         </div>
-        <div className="contact-body"></div>
+        <h1>Connect With Developers</h1>
+        <div className="contact-header">
+          <h4>Hitesh Marwaha</h4>
+          <a href="mailto:hiteshmarwaha24@gmail.com">
+            hiteshmarwaha24@gmail.com
+          </a>
+        </div>
+        <div className="contact-header">
+          <h4>Organisation</h4>
+          <a href="mailto:nameless.care@gmail.com">nameless.care@gmail.com</a>
+        </div>
+        <div className="contact-header">
+          <h4>Organisation</h4>
+          <a href="mailto:nameless.care@gmail.com">nameless.care@gmail.com</a>
+        </div>
+        <div className="contact-header">
+          <h4>Organisation</h4>
+          <a href="mailto:nameless.care@gmail.com">nameless.care@gmail.com</a>
+        </div>
       </div>
     </div>
   );
