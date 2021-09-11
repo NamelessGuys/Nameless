61e1d854ff62466487af1a4e97dc6be630290ad9
AYUSHBANSAL-hub <forstudyonly101@gmail.com>
2021-09-11 18:19:21 +0530

    contact us page updated. Card for each member added

--- frontend/src/components/user/Contact.js
+++ frontend/src/components/user/Contact.js
@@ -1,33 +1,175 @@
-import React from "react";
-import "../../css/user.css";
-
+import React from 'react';
+import '../../css/user.css';
+import { FiMail } from 'react-icons/fi';
+import { FaInstagramSquare, FaLinkedin } from 'react-icons/fa';
 const Contact = () => {
   return (
-    <div id="contact">
-      <h2 className="large">Contact Us</h2>
-      <div className="contact-content">
-        <div className="contact-header">
-          <h4>Organisation</h4>
-          <a href="mailto:nameless.care@gmail.com">nameless.care@gmail.com</a>
+    <div id='contact'>
+      <div className='contact-content'>
+        <div className='contact-header'>
+          <h2>Nameless at your service</h2>
+          <p className='contact-para'>
+            lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+            lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+            lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+            lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
+            lorem lorem lorem lorem lorem lorem lorem lorem lorem
+          </p>
+          <div class='card-social-icons'>
+            <a href='mailto:bansal.ayush2002@gmail.com'>
+              <FiMail />
+            </a>
+            <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+              <FaLinkedin />
+            </a>
+            <a href='https://www.instagram.com/ayushbansal2002/'>
+              <FaInstagramSquare />
+            </a>
+          </div>
         </div>
-        <h1>Connect With Developers</h1>
-        <div className="contact-header">
-          <h4>Hitesh Marwaha</h4>
-          <a href="mailto:hiteshmarwaha24@gmail.com">
-            hiteshmarwaha24@gmail.com
-          </a>
+        <div className='row-card'>
+          <div class='card'>
+            <div class='card-background'>
+              <img
+                src='https://lh3.googleusercontent.com/-paS9Qm_3L9E/XGK6C0wkWbI/AAAAAAAAAyg/sqwN0ovgb4oD-8cmkNYhLY67SvWZSAnbQCLcBGAs/h120/askjd.jpg'
+                class='card-image'
+              />
+            </div>
+            <div class='card-info'>
+              <h1>Ayush Bansal</h1>
+              <p>frontend developer</p>
+              <br />
+              <p>
+                Demi Lovato is a Grammy nominated and multi-platinum singer,
+                songwriter, actress.
+              </p>
+            </div>
+            <div class='card-social-icons'>
+              <a href='mailto:bansal.ayush2002@gmail.com'>
+                <FiMail />
+              </a>
+              <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+                <FaLinkedin />
+              </a>
+              <a href='https://www.instagram.com/ayushbansal2002/'>
+                <FaInstagramSquare />
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <div class='card-background'>
+              <img
+                src='https://lh3.googleusercontent.com/-paS9Qm_3L9E/XGK6C0wkWbI/AAAAAAAAAyg/sqwN0ovgb4oD-8cmkNYhLY67SvWZSAnbQCLcBGAs/h120/askjd.jpg'
+                class='card-image'
+              />
+            </div>
+            <div class='card-info'>
+              <h1>Gaurav Bansal</h1>
+              <p>backend developer</p>
+              <br />
+              <p>
+                Demi Lovato is a Grammy nominated and multi-platinum singer,
+                songwriter, actress.
+              </p>
+            </div>
+            <div class='card-social-icons'>
+              <a href='mailto:bansal.ayush2002@gmail.com'>
+                <FiMail />
+              </a>
+              <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+                <FaLinkedin />
+              </a>
+              <a href='https://www.instagram.com/ayushbansal2002/'>
+                <FaInstagramSquare />
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <div class='card-background'>
+              <img
+                src='https://lh3.googleusercontent.com/-paS9Qm_3L9E/XGK6C0wkWbI/AAAAAAAAAyg/sqwN0ovgb4oD-8cmkNYhLY67SvWZSAnbQCLcBGAs/h120/askjd.jpg'
+                class='card-image'
+              />
+            </div>
+            <div class='card-info'>
+              <h1>Hitesh Marwaha</h1>
+              <p>backend developer</p>
+              <br />
+              <p>
+                Demi Lovato is a Grammy nominated and multi-platinum singer,
+                songwriter, actress.
+              </p>
+            </div>
+            <div class='card-social-icons'>
+              <a href='mailto:bansal.ayush2002@gmail.com'>
+                <FiMail />
+              </a>
+              <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+                <FaLinkedin />
+              </a>
+              <a href='https://www.instagram.com/ayushbansal2002/'>
+                <FaInstagramSquare />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="contact-header">
-          <h4>Organisation</h4>
-          <a href="mailto:nameless.care@gmail.com">nameless.care@gmail.com</a>
-        </div>
-        <div className="contact-header">
-          <h4>Organisation</h4>
-          <a href="mailto:nameless.care@gmail.com">nameless.care@gmail.com</a>
-        </div>
-        <div className="contact-header">
-          <h4>Organisation</h4>
-          <a href="mailto:nameless.care@gmail.com">nameless.care@gmail.com</a>
+        <div className='row-card-2'>
+          <div class='card'>
+            <div class='card-background'>
+              <img
+                src='https://lh3.googleusercontent.com/-paS9Qm_3L9E/XGK6C0wkWbI/AAAAAAAAAyg/sqwN0ovgb4oD-8cmkNYhLY67SvWZSAnbQCLcBGAs/h120/askjd.jpg'
+                class='card-image'
+              />
+            </div>
+            <div class='card-info'>
+              <h1>Pushp Jain</h1>
+              <p>frontend developer</p>
+              <br />
+              <p>
+                Demi Lovato is a Grammy nominated and multi-platinum singer,
+                songwriter, actress.
+              </p>
+            </div>
+            <div class='card-social-icons'>
+              <a href='mailto:bansal.ayush2002@gmail.com'>
+                <FiMail />
+              </a>
+              <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+                <FaLinkedin />
+              </a>
+              <a href='https://www.instagram.com/ayushbansal2002/'>
+                <FaInstagramSquare />
+              </a>
+            </div>
+          </div>
+          <div class='card'>
+            <div class='card-background'>
+              <img
+                src='https://lh3.googleusercontent.com/-paS9Qm_3L9E/XGK6C0wkWbI/AAAAAAAAAyg/sqwN0ovgb4oD-8cmkNYhLY67SvWZSAnbQCLcBGAs/h120/askjd.jpg'
+                class='card-image'
+              />
+            </div>
+            <div class='card-info'>
+              <h1>Krish Chopra</h1>
+              <p>ML developer</p>
+              <br />
+              <p>
+                Demi Lovato is a Grammy nominated and multi-platinum singer,
+                songwriter, actress.
+              </p>
+            </div>
+            <div class='card-social-icons'>
+              <a href='mailto:bansal.ayush2002@gmail.com'>
+                <FiMail />
+              </a>
+              <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+                <FaLinkedin />
+              </a>
+              <a href='https://www.instagram.com/ayushbansal2002/'>
+                <FaInstagramSquare />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
