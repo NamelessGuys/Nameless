650596acbba05e0825fd2fc570cc60b46cfa7bc1
hitu69 <hiteshmarwaha24@gmail.com>
2021-09-11 19:38:00 +0530

    changed fuckedup css

--- frontend/src/components/user/Contact.js
+++ frontend/src/components/user/Contact.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import '../../css/user.css';
-import { FiMail } from 'react-icons/fi';
-import { FaInstagramSquare, FaLinkedin } from 'react-icons/fa';
+import React from "react";
+import "../../css/user.css";
+import { FiMail } from "react-icons/fi";
+import { FaInstagramSquare, FaLinkedin } from "react-icons/fa";
 const Contact = () => {
   return (
-    <div id='contact'>
-      <div className='contact-content'>
-        <div className='contact-header'>
+    <div id="contact">
+      <div className="contact-content">
+        <div className="contact-header">
           <h2>Nameless at your service</h2>
-          <p className='contact-para'>
+          <p className="contact-para">
             lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
             lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
             lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
             lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
             lorem lorem lorem lorem lorem lorem lorem lorem lorem
           </p>
-          <div className='card-social-icons'>
-            <a href='mailto:bansal.ayush2002@gmail.com'>
+          <div className="card-social-icons">
+            <a href="mailto:bansal.ayush2002@gmail.com">
               <FiMail />
             </a>
-            <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+            <a href="https://www.linkedin.com/in/ayush-bansal-50a6731b7/">
               <FaLinkedin />
             </a>
-            <a href='https://www.instagram.com/ayushbansal2002/'>
+            <a href="https://www.instagram.com/ayushbansal2002/">
               <FaInstagramSquare />
             </a>
           </div>
         </div>
-        <div className='row-card'>
-          <div className='card'>
-            <div className='card-background'>
+        <div className="row-card">
+          <div className="card">
+            <div className="card-background">
               <img
-                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/whatsapp/238/bird_1f426.png'
-                className='card-image'
+                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/whatsapp/238/bird_1f426.png"
+                className="card-image"
               />
             </div>
-            <div className='card-info'>
+            <div className="card-info">
               <h1>Ayush Bansal</h1>
               <p>frontend developer</p>
               <br />
@@ -44,26 +44,26 @@ const Contact = () => {
                 songwriter, actress.
               </p>
             </div>
-            <div className='card-social-icons'>
-              <a href='mailto:bansal.ayush2002@gmail.com'>
+            <div className="card-social-icons">
+              <a href="mailto:bansal.ayush2002@gmail.com">
                 <FiMail />
               </a>
-              <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+              <a href="https://www.linkedin.com/in/ayush-bansal-50a6731b7/">
                 <FaLinkedin />
               </a>
-              <a href='https://www.instagram.com/ayushbansal2002/'>
+              <a href="https://www.instagram.com/ayushbansal2002/">
                 <FaInstagramSquare />
               </a>
             </div>
           </div>
-          <div className='card'>
-            <div className='card-background'>
+          <div className="card">
+            <div className="card-background">
               <img
-                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/whatsapp/238/bird_1f426.png'
-                className='card-image'
+                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/whatsapp/238/bird_1f426.png"
+                className="card-image"
               />
             </div>
-            <div className='card-info'>
+            <div className="card-info">
               <h1>Gaurav Bansal</h1>
               <p>backend developer</p>
               <br />
@@ -72,26 +72,26 @@ const Contact = () => {
                 songwriter, actress.
               </p>
             </div>
-            <div className='card-social-icons'>
-              <a href='mailto:bansal.ayush2002@gmail.com'>
+            <div className="card-social-icons">
+              <a href="mailto:bansal.ayush2002@gmail.com">
                 <FiMail />
               </a>
-              <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+              <a href="https://www.linkedin.com/in/ayush-bansal-50a6731b7/">
                 <FaLinkedin />
               </a>
-              <a href='https://www.instagram.com/ayushbansal2002/'>
+              <a href="https://www.instagram.com/ayushbansal2002/">
                 <FaInstagramSquare />
               </a>
             </div>
           </div>
-          <div className='card'>
-            <div className='card-background'>
+          <div className="card">
+            <div className="card-background">
               <img
-                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/whatsapp/238/bird_1f426.png'
-                className='card-image'
+                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/whatsapp/238/bird_1f426.png"
+                className="card-image"
               />
             </div>
-            <div className='card-info'>
+            <div className="card-info">
               <h1>Hitesh Marwaha</h1>
               <p>backend developer</p>
               <br />
@@ -100,28 +100,28 @@ const Contact = () => {
                 songwriter, actress.
               </p>
             </div>
-            <div className='card-social-icons'>
-              <a href='mailto:bansal.ayush2002@gmail.com'>
+            <div className="card-social-icons">
+              <a href="mailto:bansal.ayush2002@gmail.com">
                 <FiMail />
               </a>
-              <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+              <a href="https://www.linkedin.com/in/ayush-bansal-50a6731b7/">
                 <FaLinkedin />
               </a>
-              <a href='https://www.instagram.com/ayushbansal2002/'>
+              <a href="https://www.instagram.com/ayushbansal2002/">
                 <FaInstagramSquare />
               </a>
             </div>
           </div>
         </div>
-        <div className='row-card-2'>
-          <div className='card'>
-            <div className='card-background'>
+        <div className="row-card-2">
+          <div className="card">
+            <div className="card-background">
               <img
-                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/whatsapp/238/bird_1f426.png'
-                className='card-image'
+                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/whatsapp/238/bird_1f426.png"
+                className="card-image"
               />
             </div>
-            <div className='card-info'>
+            <div className="card-info">
               <h1>Pushp Jain</h1>
               <p>frontend developer</p>
               <br />
@@ -130,26 +130,26 @@ const Contact = () => {
                 songwriter, actress.
               </p>
             </div>
-            <div className='card-social-icons'>
-              <a href='mailto:bansal.ayush2002@gmail.com'>
+            <div className="card-social-icons">
+              <a href="mailto:bansal.ayush2002@gmail.com">
                 <FiMail />
               </a>
-              <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+              <a href="https://www.linkedin.com/in/ayush-bansal-50a6731b7/">
                 <FaLinkedin />
               </a>
-              <a href='https://www.instagram.com/ayushbansal2002/'>
+              <a href="https://www.instagram.com/ayushbansal2002/">
                 <FaInstagramSquare />
               </a>
             </div>
           </div>
-          <div className='card'>
-            <div className='card-background'>
+          <div className="card">
+            <div className="card-background">
               <img
-                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/whatsapp/238/bird_1f426.png'
-                className='card-image'
+                src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/whatsapp/238/bird_1f426.png"
+                className="card-image"
               />
             </div>
-            <div className='card-info'>
+            <div className="card-info">
               <h1>Krish Chopra</h1>
               <p>ML developer</p>
               <br />
@@ -158,14 +158,14 @@ const Contact = () => {
                 songwriter, actress.
               </p>
             </div>
-            <div className='card-social-icons'>
-              <a href='mailto:bansal.ayush2002@gmail.com'>
+            <div className="card-social-icons">
+              <a href="mailto:bansal.ayush2002@gmail.com">
                 <FiMail />
               </a>
-              <a href='https://www.linkedin.com/in/ayush-bansal-50a6731b7/'>
+              <a href="https://www.linkedin.com/in/ayush-bansal-50a6731b7/">
                 <FaLinkedin />
               </a>
-              <a href='https://www.instagram.com/ayushbansal2002/'>
+              <a href="https://www.instagram.com/ayushbansal2002/">
                 <FaInstagramSquare />
               </a>
             </div>
