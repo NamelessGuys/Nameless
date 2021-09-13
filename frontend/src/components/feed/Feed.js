7d7493737a08bf79a669b24d7ab5b522782c5a53
AYUSHBANSAL-hub <forstudyonly101@gmail.com>
2021-09-13 16:21:24 +0530

    upload button added

--- frontend/src/components/feed/Feed.js
+++ frontend/src/components/feed/Feed.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Posts from './Posts';
 import Filters from './Filters';
 import '../../css/feed.css';
+import { BsPlusCircleFill } from 'react-icons/bs';
 
 const Feed = () => {
   return (
-    <div id="feed">
-      <Posts />
-      <Filters />
-    </div>
+    <Fragment>
+      <button type='button' className='upload-btn btn'>
+        <BsPlusCircleFill />
+      </button>
+      <div id='feed'>
+        <Posts />
+        <Filters />
+      </div>
+    </Fragment>
   );
 };
 
