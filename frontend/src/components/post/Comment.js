d45a0b68c107f41649897b837e1fe79f5bf25bdb
Gaurav Bansal <bansalgaurav726@gmail.com>
2021-09-16 21:24:07 +0530

    Made Comment Section

--- frontend/src/components/post/Comment.js
+++ frontend/src/components/post/Comment.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const Comment = () => {
   return (
-    <div className='single-comment'>
-      <div className='comment-avatar'></div>
-      <div className='comment-message'></div>
+    <div className="single-comment">
+      <div className="comment-avatar">
+        <img src="https://randomuser.me/api/portraits/men/1.jpg" alt="User" />
+      </div>
+      <div className="comment-content">
+        <div className="comment-user">
+          <h4>John Doe</h4>
+        </div>
+        <div className="comment-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          doloremque ut natus, maiores velit ea magni placeat,
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/components/post/Comments.js
+++ frontend/src/components/post/Comments.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import Comment from './Comment';
+import { IoMdSend } from 'react-icons/io';
 
 const Comments = () => {
   return (
-    <div className='comment-box'>
-      <div className='comments'>
+    <div className="comment-box">
+      <div className="comments">
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
         <Comment />
         <Comment />
         <Comment />
         <Comment />
       </div>
-      <div className='add-comment'>
-        <label>
-          <textarea placeholder='Add comment(max limit:500)'></textarea>
-        </label>
+      <div className="add-comment">
+        <form>
+          <textarea className="comment-input"></textarea>
+          <button className="send-btn">
+            <IoMdSend />
+          </button>
+        </form>
       </div>
     </div>
   );
