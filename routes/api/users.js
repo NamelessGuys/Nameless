d4fe9647638c96cd6bbf52951d33f7c15d82ac61
Gaurav Bansal <bansalgaurav726@gmail.com>
2021-09-25 19:10:58 +0530

    Added user and auth routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,33 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-const auth = require('../../middleware/auth');
 const config = require('config');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
 
-// @route     GET api/auth
-// @desc      Get user
-// @access    Private
-router.get('/', auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-// @route     POST api/auth
-// @desc      Login user
+// @route     POST api/users
+// @desc      Register user
 // @access    Public
 router.post(
   '/',
   [
+    check('username', 'Please enter a username').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter password').exists(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -35,24 +25,32 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { email, password } = req.body;
+    const { username, email, password, college } = req.body;
 
     try {
       // See if user exists
       let user = await User.findOne({ email });
-      if (!user) {
+      if (user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
-      // Check password
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'Invalid Credentials' }] });
-      }
+      // Generate random avatar
+      const avatar = `https://avatars.dicebear.com/api/croodles/${username}.svg`;
+
+      user = new User({
+        username,
+        email,
+        password,
+        avatar,
+        college,
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
 
       // Return jsonwebtoken
       const payload = {
--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(express.json({ extended: false }));
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define routes
-// app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
