34eea83da42f4995c686c6286776ef471587b181
Pushp Jain <77234511+pushpjain3@users.noreply.github.com>
2021-09-27 23:55:19 +0530

    modal responsive

--- frontend/src/components/feed/AddPostModal.js
+++ frontend/src/components/feed/AddPostModal.js
@@ -1,49 +1,50 @@
-import React from 'react';
-import '../../css/feed.css';
-import { FiUpload } from 'react-icons/fi';
+import React from "react";
+import "../../css/feed.css";
+import { FiUpload } from "react-icons/fi";
 
 const AddPostModal = () => {
   return (
-    <div id='add-post'>
-      <div className='modal-header'>
-        <h2 className='large'>Upload New Post</h2>
-        <button className='btn btn-danger'>X</button>
+    <div id="add-post">
+      <div className="modal-header">
+        <h2 className="large">Upload New Post</h2>
+        <button className="btn btn-danger">X</button>
       </div>
-      <div className='line'></div>
-      <div className='modal-body'>
-        <form className='form'>
+      <div className="line"></div>
+      <div className="modal-body">
+        <form className="form">
           <label>
             <input
-              className='modal-input'
-              type='text'
-              placeholder='Title'
+              className="modal-input"
+              type="text"
+              placeholder="Title"
             ></input>
           </label>
           <label>
             <textarea
-              className='modal-input input-large'
-              placeholder='Text'
+              className="modal-input input-large"
+              placeholder="Text"
             ></textarea>
           </label>
-          <div className='modal-body-footer'>
+          <div className="modal-body-footer">
             <label>
               <input
-                className='modal-input tag'
-                type='text'
-                placeholder='#tag1, #tag2,.... (max 5)'
+                className="modal-input tag"
+                type="text"
+                placeholder="#tag1, #tag2,.... (max 5)"
               ></input>
             </label>
             <label>
-              <div className='btn upload-img'>
-                <FiUpload /> Upload Image
-                <input type='file' className='modal-upload-image' />
+              <div className="btn upload-img">
+                <FiUpload />
+                <span> Upload Image</span>
+                <input type="file" className="modal-upload-image" />
               </div>
             </label>
           </div>
         </form>
       </div>
-      <div className='modal-footer'>
-        <button className='btn btn-success'>Add Post</button>
+      <div className="modal-footer">
+        <button className="btn btn-success">Add Post</button>
       </div>
     </div>
   );
