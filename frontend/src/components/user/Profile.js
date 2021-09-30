4c75bf49baa6ed55d3b3d9c46cf1e53ce875dc55
AYUSHBANSAL-hub <forstudyonly101@gmail.com>
2021-09-30 20:56:42 +0530

    register page,login,settings page made responsive

--- frontend/src/components/user/Profile.js
+++ frontend/src/components/user/Profile.js
@@ -1,61 +1,58 @@
-import React from "react";
-import Carousel from "react-elastic-carousel";
-import Item from "./Item";
+import React from 'react';
+import Carousel from 'react-elastic-carousel';
+import Item from './Item';
 
 const Profile = () => {
   const items = [1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
-    <div id="profile" className="bg-dark">
-      <div className="profile-head">
-        <div className="profile-avatar">
+    <div id='profile' className='bg-dark'>
+      <div className='profile-head'>
+        <div className='profile-avatar'>
           <img
-            src="https://randomuser.me/api/portraits/men/1.jpg"
-            alt="User Avatar"
+            src='https://randomuser.me/api/portraits/men/1.jpg'
+            alt='User Avatar'
           />
-          <button className="change-avatar btn btn-primary">
-            Generate new avatar
-          </button>
         </div>
-        <div className="profile-user">
+        <div className='profile-user'>
           <h3>Username</h3>
           <p>Date Joined: 04/11/2021</p>
         </div>
-        <button class="profile-college blob-btn">
+        <button class='profile-college blob-btn'>
           DTU
-          <span class="blob-btn__inner">
-            <span class="blob-btn__blobs">
-              <span class="blob-btn__blob"></span>
-              <span class="blob-btn__blob"></span>
-              <span class="blob-btn__blob"></span>
-              <span class="blob-btn__blob"></span>
+          <span class='blob-btn__inner'>
+            <span class='blob-btn__blobs'>
+              <span class='blob-btn__blob'></span>
+              <span class='blob-btn__blob'></span>
+              <span class='blob-btn__blob'></span>
+              <span class='blob-btn__blob'></span>
             </span>
           </span>
         </button>
       </div>
-      <div className="profile-score">
-        <div className="profile-rank">
+      <div className='profile-score'>
+        <div className='profile-rank'>
           <img
-            src="https://randomuser.me/api/portraits/men/1.jpg"
-            alt="User Avatar"
+            src='https://randomuser.me/api/portraits/men/1.jpg'
+            alt='User Avatar'
           />
         </div>
-        <div className="profile-counter">
-          <div className="counter">
+        <div className='profile-counter'>
+          <div className='counter'>
             <small>Total Posts</small>
             <h2>0</h2>
           </div>
-          <div className="counter">
+          <div className='counter'>
             <small>Total Comemnts</small>
             <h2>0</h2>
           </div>
-          <div className="counter">
+          <div className='counter'>
             <small>Total Votes</small>
             <h2>0</h2>
           </div>
         </div>
       </div>
-      <div className="profile-badges">
+      <div className='profile-badges'>
         <Carousel
           itemsToShow={3}
           itemsToScroll={1}
@@ -64,10 +61,10 @@ const Profile = () => {
         >
           {items.map((item) => (
             <Item key={item}>
-              {" "}
+              {' '}
               <img
-                src="https://randomuser.me/api/portraits/men/1.jpg"
-                alt="User Avatar"
+                src='https://randomuser.me/api/portraits/men/1.jpg'
+                alt='User Avatar'
               />
             </Item>
           ))}
